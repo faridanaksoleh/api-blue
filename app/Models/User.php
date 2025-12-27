6a44924643d79f932090f3f78939189f22a4848d
faridanaksoleh <itspuyd@gmail.com>
2025-12-27 10:03:14 +0700

    feat: model dan migration Store

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Traits\UUID;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,7 +12,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, UUID;
 
     /**
      * The attributes that are mass assignable.
@@ -45,4 +47,11 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    // user can have one store relationship
+    public function store()
+    {
+        return $this->hasOne(Store::class);
+    }
+
 }
