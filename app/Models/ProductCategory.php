327d726111c585f77f74416a51d2a71f517120e0
faridanaksoleh <itspuyd@gmail.com>
2025-12-28 14:09:50 +0700

    feat: model dan migration ProductCategory

---
+++ app/Models/ProductCategory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UUID;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategory extends Model
+{
+    use UUID;
+
+    protected $fillable = [
+        'parent_id',
+        'image',
+        'name',
+        'slug',
+        'tagline',
+        'description',
+    ];
+
+    public function parent()
+    {
+        return $this->belongTo(ProductCategory::class, 'parent_id', 'id');
+    } 
+    
+    public function childrens()
+    {
+        return $this->hasMany(ProductCategory::class, 'parent_id', 'id');
+    }
+}
