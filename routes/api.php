3a95da2313f0cb75fb03aa143a950350b877d401
faridanaksoleh <itspuyd@gmail.com>
2025-12-31 17:28:03 +0700

    fix: typo users to user, sekalian testing Get All Pada Postman

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('users', UserController::class);
+Route::apiResource('user', UserController::class);
 Route::get('user/all/paginated', [UserController::class, 'getAllPaginated']);
